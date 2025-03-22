ee02b11f11a076530b4f29eb33ad700fa2c00235
ShevtsovLA <carbajot@gmail.com>
2025-03-22 17:09:59 +0300

    Домашка по компоуз

---
+++ app/src/androidTest/kotlin/com/google/samples/apps/nowinandroid/ui/homeworks/homework14/MainScreen.kt
@@ -0,0 +1,8 @@
+package com.google.samples.apps.nowinandroid.ui.homeworks.homework14
+
+import androidx.compose.ui.test.SemanticsNodeInteractionsProvider
+import io.github.kakaocup.compose.node.element.ComposeScreen
+
+class MainScreen(semanticsProvider: SemanticsNodeInteractionsProvider) :
+    ComposeScreen<MainScreen>(semanticsProvider) {
+}
